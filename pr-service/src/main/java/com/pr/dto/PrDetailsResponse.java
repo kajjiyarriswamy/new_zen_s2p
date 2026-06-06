a8efaaa7d2a3dfe08e185d88e1c08842b5c0ed6e
hruday-dev <thirumalahrudayboda@gmail.com>
2026-06-06 10:29:37 +0530

    added PrData

--- pr-service/src/main/java/com/pr/dto/PrDetailsResponse.java
+++ pr-service/src/main/java/com/pr/dto/PrDetailsResponse.java
@@ -1,99 +1,32 @@
 package com.pr.dto;
 
-public class PrDetailsResponse {
-    private String prNumber;
-    private String prDescription;
-    private String requestor;
-    private String prStatus;
+public class PRDetailsResponse {
+	 private String prDescription;
+	    private String budgetId;
+	    private String budgetName;
 
-    private String budgetId;
-    private String budgetName;
+	    public String getPrDescription() {
+	        return prDescription;
+	    }
 
-    private String poNumber;
-    private String poDescription;
-    private String poStatus;
+	    public void setPrDescription(String prDescription) {
+	        this.prDescription = prDescription;
+	    }
 
-    private Double amount;
+	    public String getBudgetId() {
+	        return budgetId;
+	    }
 
-	public String getPrNumber() {
-		return prNumber;
+	    public void setBudgetId(String budgetId) {
+	        this.budgetId = budgetId;
+	    }
+
+	    public String getBudgetName() {
+	        return budgetName;
+	    }
+
+	    public void setBudgetName(String budgetName) {
+	        this.budgetName = budgetName;
+	    }
 	}
 
-	public void setPrNumber(String prNumber) {
-		this.prNumber = prNumber;
-	}
-
-	public String getPrDescription() {
-		return prDescription;
-	}
-
-	public void setPrDescription(String prDescription) {
-		this.prDescription = prDescription;
-	}
-
-	public String getRequestor() {
-		return requestor;
-	}
-
-	public void setRequestor(String requestor) {
-		this.requestor = requestor;
-	}
-
-	public String getPrStatus() {
-		return prStatus;
-	}
-
-	public void setPrStatus(String prStatus) {
-		this.prStatus = prStatus;
-	}
-
-	public String getBudgetId() {
-		return budgetId;
-	}
-
-	public void setBudgetId(String budgetId) {
-		this.budgetId = budgetId;
-	}
-
-	public String getBudgetName() {
-		return budgetName;
-	}
-
-	public void setBudgetName(String budgetName) {
-		this.budgetName = budgetName;
-	}
-
-	public String getPoNumber() {
-		return poNumber;
-	}
-
-	public void setPoNumber(String poNumber) {
-		this.poNumber = poNumber;
-	}
-
-	public String getPoDescription() {
-		return poDescription;
-	}
-
-	public void setPoDescription(String poDescription) {
-		this.poDescription = poDescription;
-	}
-
-	public String getPoStatus() {
-		return poStatus;
-	}
-
-	public void setPoStatus(String poStatus) {
-		this.poStatus = poStatus;
-	}
-
-	public Double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-
-}
