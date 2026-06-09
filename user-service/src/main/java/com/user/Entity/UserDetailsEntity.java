377480535b47875d97b9812e48e9f8121002b9ee
Akashpandav44 <pandava34@gmail.com>
2026-06-09 17:16:33 +0530

    Added S3 document upload functionality for user service

---
+++ user-service/src/main/java/com/user/Entity/UserDetailsEntity.java
@@ -0,0 +1,219 @@
+package com.user.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="user_Details")
+public class UserDetailsEntity {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+	 @Column(name = "name")
+	    private String name;
+
+	    @Column(name = "user_name")
+	    private String userName;
+	    
+	    @Column(name = "user_id")
+	    private String userId;
+
+	    @Column(name = "designation")
+	    private String designation;
+
+	    @Column(name = "email_id")
+	    private String emailId;
+
+	    @Column(name = "phone")
+	    private String phone;
+
+	    @Column(name = "address")
+	    private String address;
+
+	    @Column(name = "otp")
+	    private String otp;
+
+	    @Column(name = "user_status")
+	    private String userStatus;
+
+	    @Column(name = "user_type")
+	    private String userType;
+
+	    @Column(name = "login_failed_attempts")
+	    private int loginFailedAttempts;
+
+	    @Column(name = "locked_duration_time")
+	    private String lockedDurationTime;
+
+	    @Column(name = "created_time")
+	    private String createdTime;
+
+	    @Column(name = "created_by")
+	    private String createdBy;
+
+	    @Column(name = "last_updated_time")
+	    private String lastUpdatedTime;
+
+	    @Column(name = "last_updated_by")
+	    private String lastUpdatedBy;
+	    
+	    @Column(name = "document_path")
+	    private String documentPath;
+
+	    public UserDetailsEntity() {
+	    }
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		public String getDesignation() {
+			return designation;
+		}
+
+		public void setDesignation(String designation) {
+			this.designation = designation;
+		}
+
+		public String getEmailId() {
+			return emailId;
+		}
+
+		public void setEmailId(String emailId) {
+			this.emailId = emailId;
+		}
+
+		public String getPhone() {
+			return phone;
+		}
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public String getOtp() {
+			return otp;
+		}
+
+		public void setOtp(String otp) {
+			this.otp = otp;
+		}
+		
+
+		public String getUserId() {
+			return userId;
+		}
+
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+
+		public String getUserStatus() {
+			return userStatus;
+		}
+
+		public void setUserStatus(String userStatus) {
+			this.userStatus = userStatus;
+		}
+
+		public String getUserType() {
+			return userType;
+		}
+
+		public void setUserType(String userType) {
+			this.userType = userType;
+		}
+
+		public int getLoginFailedAttempts() {
+			return loginFailedAttempts;
+		}
+
+		public void setLoginFailedAttempts(int loginFailedAttempts) {
+			this.loginFailedAttempts = loginFailedAttempts;
+		}
+
+		public String getLockedDurationTime() {
+			return lockedDurationTime;
+		}
+
+		public void setLockedDurationTime(String lockedDurationTime) {
+			this.lockedDurationTime = lockedDurationTime;
+		}
+
+		public String getCreatedTime() {
+			return createdTime;
+		}
+
+		public void setCreatedTime(String createdTime) {
+			this.createdTime = createdTime;
+		}
+
+		public String getCreatedBy() {
+			return createdBy;
+		}
+
+		public void setCreatedBy(String createdBy) {
+			this.createdBy = createdBy;
+		}
+
+		public String getLastUpdatedTime() {
+			return lastUpdatedTime;
+		}
+
+		public void setLastUpdatedTime(String lastUpdatedTime) {
+			this.lastUpdatedTime = lastUpdatedTime;
+		}
+
+		public String getLastUpdatedBy() {
+			return lastUpdatedBy;
+		}
+
+		public void setLastUpdatedBy(String lastUpdatedBy) {
+			this.lastUpdatedBy = lastUpdatedBy;
+		}
+
+		public String getDocumentPath() {
+			return documentPath;
+		}
+
+		public void setDocumentPath(String documentPath) {
+			this.documentPath = documentPath;
+		}
+		
+	    
+}
--- user-service/src/main/java/com/user/controller/UserMappingController.java
+++ user-service/src/main/java/com/user/controller/UserMappingController.java
@@ -1,24 +1,55 @@
 package com.user.controller;
 
+import com.user.Entity.UserDetailsEntity;
 import com.user.dto.ApiResponse;
 import com.user.dto.OrgMap;
 import com.user.dto.RoleMap;
+import com.user.dto.UserDetails;
 import com.user.dto.UserVendorMap;
+import com.user.kafka.KafkaProducer;
+import com.user.repository.UserMappingRepository;
 import com.user.service.UserMappingService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.user.storage.S3StorageService;
 
+import org.apache.kafka.common.Uuid;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user/mapping")
 public class UserMappingController {
+	
+	private static final org.slf4j.Logger Logger= org.slf4j.LoggerFactory.getLogger(UserMappingController.class);
+	
+	private final KafkaProducer kafkaProducer;
+	private final UserMappingRepository userMappingRepository;
+	private final com.user.storage.S3StorageService s3StorageService;
+	private final UserMappingService userMappingService;
 
-    private final UserMappingService userMappingService;
-
-    public UserMappingController(UserMappingService userMappingService) {
-        this.userMappingService = userMappingService;
+    public UserMappingController(KafkaProducer kafkaProducer, UserMappingRepository userMappingRepository, com.user.storage.S3StorageService s3StorageService,
+    		UserMappingService userMappingService) {
+    	
+    	this.kafkaProducer = kafkaProducer;
+    	this.userMappingRepository = userMappingRepository;
+    	this.s3StorageService = s3StorageService;
+    	this.userMappingService = userMappingService;
     }
+
+   
 
     @GetMapping("/org/{userId}")
     public ResponseEntity<ApiResponse<List<OrgMap>>> getOrgMappings(@PathVariable String userId) {
@@ -57,5 +88,81 @@ public class UserMappingController {
                                                                        @PathVariable String vendorId) {
         ApiResponse<UserVendorMap> resp = userMappingService.linkUserToVendor(userId, vendorId);
         return ResponseEntity.status(resp.getStatusCode()).body(resp);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<UserDetailsEntity>> createUser(
+            @RequestBody UserDetailsEntity user) {
+
+        try {
+
+            UserDetailsEntity savedUser = userMappingRepository.save(user);
+
+            return ResponseEntity.ok(
+                    ApiResponse.success(savedUser,
+                            "User created successfully"));
+
+        } catch (Exception e) {
+
+            Logger.error("Failed to create user", e);
+
+            return ResponseEntity.status(500)
+                    .body(ApiResponse.internalError(
+                            "Failed to create user: " + e.getMessage()));
+        }
+    }
+    @PostMapping("/{userId}/upload-document")
+    public ResponseEntity<ApiResponse<UserDetailsEntity>> uploadUserDocument(
+            @PathVariable String userId,
+            @RequestParam("file") MultipartFile file) {
+
+        Logger.info("Uploading document for User {}", userId);
+
+        if (userId == null || userId.isEmpty()) {
+            return ResponseEntity.badRequest()
+                    .body(ApiResponse.badRequest("User id is Required"));
+        }
+
+        if (file == null || file.isEmpty()) {
+            return ResponseEntity.badRequest()
+                    .body(ApiResponse.badRequest("Document File is required"));
+        }
+
+        Optional<UserDetailsEntity> opt =
+                userMappingRepository.findByUserId(userId);
+
+        if (!opt.isPresent()) {
+            return ResponseEntity.status(404)
+                    .body(ApiResponse.notFound("User Not Found"));
+        }
+
+        try {
+
+            String objectKey = String.format(
+                    "user/%s/%s_%s",
+                    userId,
+                    UUID.randomUUID(),
+                    file.getOriginalFilename());
+
+            String documentPath =
+                    s3StorageService.uploadFile(file, objectKey);
+
+            UserDetailsEntity entity = opt.get();
+
+            entity.setDocumentPath(documentPath);
+            entity.setLastUpdatedTime(LocalDateTime.now().toString());
+
+            userMappingRepository.save(entity);
+
+            return ResponseEntity.ok(ApiResponse.success(entity, "Document upload successfully"));
+
+        } catch (Exception e) {
+
+            Logger.error("Failed to upload user document", e);
+
+            return ResponseEntity.status(500)
+                    .body(ApiResponse.internalError(
+                            "Failed to upload user document: "
+                                    + e.getMessage()));
+        }
     }
 }
